44597402685d4cda92c9975640af10141213fa77
amdrea <amndrea30@gmail.com>
2024-08-23 09:50:56 +0200

    pre prod

---
+++ config_server/conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/home/gcouser/Scrivania/prod/prod/prodPrivateGPT/myenv/bin/gunicorn'
+pythonpath = '/home/gcouser/Scrivania/prod/prodPrivateGPT'
+bind = '10.1.1.109:8000'
+workers = 3
---
+++ config_server/proc/conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/home/gcouser/Progetti/provisionenv/bin/gunicorn'
+pythonpath = '/home/gcouser/Progetti/provision2'
+bind = '10.1.1.109:8000'
+workers = 3
